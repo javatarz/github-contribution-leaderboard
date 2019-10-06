1a01de7e8e711065459ea509c74646dfbfa8d569
Karun Japhet <karun@japhet.in>
2019-10-06 14:39:47 +0530

    Always print summary score

--- client/models/user_stats.py
+++ client/models/user_stats.py
@@ -5,14 +5,14 @@ class UserStats:
         self._score = score
 
     def leaderboard_data(self) -> str:
-        return f"{self._user_id} - Score: {self._score} (PRs: {len(self._prs)})"
+        return f"User: {self._user_id} - Score: {self._score} (PRs: {len(self._prs)})"
 
     def prs_data(self) -> str:
         pr_summaries = [f"  {pr.summary()}" for pr in self._prs]
         new_line = '\n'
         pr_summary_data = new_line.join(pr_summaries)
 
-        return f"User: {self._user_id}{new_line}{pr_summary_data}{new_line}"
+        return f"User: {self._user_id} - Score: {self._score}{new_line}{pr_summary_data}{new_line}"
 
     def all_data(self) -> str:
         return self.prs_data()
