eea93f1c6f03890ddaeae43424f88786be4d287c
Karun Japhet <karun@japhet.in>
2019-10-13 14:09:32 +0530

    Run linting through pipenv

--- lint.sh
+++ lint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "flake8: Syntax errors and undefined names"
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+pipenv run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 result_1=$?
 
 echo
 echo "flake8: Syntax linting"
-flake8 . --count --max-complexity=10 --max-line-length=127 --statistics
+pipenv run flake8 . --count --max-complexity=10 --max-line-length=127 --statistics
 result_2=$?
 
 result=$(( $result_1 + $result_2 * 2 ))
