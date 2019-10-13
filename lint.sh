8415958b8dd51d35a5be583ea88764cbdf4e551c
Karun Japhet <karun@japhet.in>
2019-10-13 15:18:04 +0530

    Consolidate flake8 runs

--- lint.sh
+++ lint.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 echo "flake8: Syntax errors and undefined names"
-pipenv run flake8 . --select=E9,F63,F7,F82 --show-source --statistics
+pipenv run flake8 . --show-source --statistics --max-complexity=10 --max-line-length=127 --statistics
 result_1=$?
-
-echo
-echo "flake8: Syntax linting"
-pipenv run flake8 . --max-complexity=10 --max-line-length=127 --statistics
-result_2=$?
 
 echo
 echo "pycodestyle"
 pipenv run pycodestyle .
-result_3=$?
+result_2=$?
 
-result=$(( $result_1 + $result_2 * 2 + $result_3 * 5))
+result=$(( $result_1 + $result_2 * 2))
 exit $result
