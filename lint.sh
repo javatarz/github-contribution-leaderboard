e527bfd4749c65b579cb7008cce9590b8b10845a
Karun Japhet <karun@japhet.in>
2019-10-13 15:40:30 +0530

    Consolidate all linting to flake8
    Flake8 internally runs pycodestyle

--- lint.sh
+++ lint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo "flake8: Syntax errors and undefined names"
-pipenv run flake8 . --show-source --statistics --max-complexity=10 --max-line-length=127 --statistics
-result_1=$?
+set -e
 
-echo
-echo "pycodestyle"
-pipenv run pycodestyle .
-result_2=$?
-
-result=$(( $result_1 + $result_2 * 2))
-exit $result
+pipenv run flake8 . --select C,E,F,W --show-source --statistics --max-complexity=10 --statistics
