e72c650e6c5ab57c6b43bd0a649af926609559c2
rahulb <rahulb@sahajsoft.com>
2019-10-19 11:43:27 +0530

    Request based on pagination

--- ghcl/github_stats.py
+++ ghcl/github_stats.py
@@ -16,11 +16,11 @@ class GithubStats:
         params = dict(
             q=f'is:pr author:{user_name} archived:false',
             sort='created',
-            order='desc',
+            order='asc',
             per_page='100'
         )
 
-        items = self._request(url=url, params=params)['items']
+        items = self._paginated_request(url=url, params=params)['items']
         prs = Pool(request_parallelization_count).map(
             self._to_pull_request, items)
 
@@ -34,16 +34,26 @@ class GithubStats:
 
     def user_id_from_name(self, user_name: str) -> str:
         url = f"https://api.github.com/users/{user_name}"
-        return self._request(url)['id']
+        return self._request(url).json()['id']
 
     def _to_pull_request(self, issues_data: dict) -> PullRequest:
-        pr_data = self._request(issues_data['pull_request']['url'])
+        pr_data = self._request(issues_data['pull_request']['url']).json()
         all_data = {'issues_data': {**issues_data}, 'pr_data': {**pr_data}}
         return PullRequest(**all_data)
+
+    def _paginated_request(self, url: str, params: dict = None) -> dict:
+        response = self._request(url, params)
+        aggregate_response = response.json()['items']
+        while 'next' in response.links.keys():
+            response = requests.get(response.links['next']['url'], params)
+            if 'items' in response.json().keys():
+                aggregate_response += (response.json()['items'])
+        res = {'items': aggregate_response}
+        return res
 
     def _request(self, url: str, params: dict = None) -> dict:
         return requests.get(
             url=url,
             params=params,
             headers=self._request_headers
-        ).json()
+        )
