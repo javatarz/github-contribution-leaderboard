3b4a6f68d595794010f4472feb5d0a2d65996972
Karun Japhet <karun@japhet.in>
2019-10-20 08:52:50 +0530

    Print overall stats

--- stats.py
+++ stats.py
@@ -11,5 +11,24 @@ contribs = Contributions(
 stats = contribs.leaderboard(
     args['users'], args['start_date'], args['end_date'])
 
+print("===========================")
+print("Leaderboard")
+print("===========================")
 for stat in stats:
     print(getattr(stat, f"{args['mode']}_data")())
+print("===========================")
+
+if args['with_summary']:
+    print()
+    print("===========================")
+    print("Summary")
+    print("===========================")
+    print(f"Users:")
+    user_count = len(args['users'])
+    print(f"  count: {user_count}")
+    print("PRs:")
+    in_period_pr_count = sum([stat.pr_count() for stat in stats])
+    print(f"  during period: {in_period_pr_count}")
+    total_pr_count = sum([stat.total_count for stat in stats])
+    print(f"  overall: {total_pr_count}")
+    print("===========================")
