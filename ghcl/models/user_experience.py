545a600456510706b9ecb8397decd9db9aee7009
Karun Japhet <karun@japhet.in>
2019-10-20 12:14:44 +0530

    Display past user experience and current performance

--- ghcl/models/user_experience.py
+++ ghcl/models/user_experience.py
@@ -19,3 +19,16 @@ class UserPRExperience(Enum):
 class OutOfRangeException(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class ExperienceSummary:
+    def __init__(self, user_experience: UserPRExperience,
+                 people_count: int,
+                 pr_count: int):
+        self._user_experience = user_experience
+        self._people_count = people_count
+        self._pr_count = pr_count
+
+    def summary(self) -> str:
+        result = f'{self._pr_count} PRs by {self._people_count} people'
+        return f"  {self._user_experience.name}: {result}"
