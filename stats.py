c9de6f16e08dd21b95bc6ad54e01185e58084e84
Karun Japhet <karun@japhet.in>
2019-10-20 12:14:44 +0530

    Display past user experience and current performance

--- stats.py
+++ stats.py
@@ -1,7 +1,41 @@
 
-from utils.args import parse_args
+from typing import List
+
 from ghcl.contributions import Contributions
 from ghcl.github_stats import GithubStats
+from ghcl.models.user_experience import UserPRExperience, ExperienceSummary
+from ghcl.models.user_stats import UserStats
+from utils.args import parse_args
+
+
+def _stats_with_exp(e: UserPRExperience) -> (UserPRExperience, List[int]):
+    return (e, [stat.pr_count() for stat in stats])
+
+
+def _stats_to_prs(t: (UserPRExperience, List[int])) -> (UserPRExperience, int):
+    return (t[0], sum(t[1]))
+
+
+def _experience_data(stats: List[UserStats]) -> str:
+    experiences = [stat.user_past_pr_experience() for stat in stats]
+
+    def _to_summary_experience(t: (UserPRExperience, int)) -> str:
+        return ExperienceSummary(
+            user_experience=t[0],
+            people_count=experiences.count(t[0]),
+            pr_count=t[1]
+        ).summary()
+
+    past_summaries = map(
+        _to_summary_experience,
+        map(
+            _stats_to_prs,
+            map(_stats_with_exp, experiences)
+        )
+    )
+
+    return '\n'.join(past_summaries)
+
 
 args = parse_args()
 contribs = Contributions(
@@ -31,4 +65,6 @@ if args['with_summary']:
     print(f"  during period: {in_period_pr_count}")
     total_pr_count = sum([stat.total_count for stat in stats])
     print(f"  overall: {total_pr_count}")
+    print("User experience:")
+    print(_experience_data(stats))
     print("===========================")
